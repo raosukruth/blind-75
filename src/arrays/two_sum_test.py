f89e62a72dab0f87552dc538d916e5ef81bddab1
Sukruth Rao <sukruthrao@MacBookPro.lan>
2026-02-08 20:44:25 -0800

    Added 2 more test cases for two_sum.py

--- src/arrays/two_sum_test.py
+++ src/arrays/two_sum_test.py
@@ -4,3 +4,13 @@ from arrays.two_sum import two_sum
 def test_two_sum():
     assert two_sum([2, 7, 11, 15], 9) == (0, 1)
     assert two_sum([2, 4, 1000, 0], 1002) == (0, 2)
+
+
+def test_multiple_zeroes_target_zero():
+    nums = [0, 0, 0]
+    assert two_sum(nums, 0) == (0, 1)
+
+
+def test_non_adjacent_pair_found():
+    nums = [5, 2, 3, 1, 4]
+    assert two_sum(nums, 6) == (0, 3)
